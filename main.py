f5e4bf9fd675f8b50dcba5daa67123c031042364
mishra <jisumishra4@gmail.com>
2026-05-23 00:29:49 +0530

    Make Talking Dynamic

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import pyttsx3
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
-engine.say('command')
-engine.say('I Am Your Alxa')
-engine.say('What Can I Do For You')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 try:
     with sr.Microphone() as source:
         print("Listening.......")
@@ -14,7 +17,7 @@ try:
         command = listener.recognize_google(voice)
         command = command.lower()
         if 'alexa' in command:
-            print(command)
+            talk(command)
 
 except Exception as e:
     print(e)
