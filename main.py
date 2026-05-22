dc0f54b468b94338b5e1d8bc9e7127507315369b
mishra <jisumishra4@gmail.com>
2026-05-23 02:55:37 +0530

    Import Packages And Remove Some Errors And Exceptions

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
+from token import COMMA
+
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
+
+
 def talk(text):
     engine.say(text)
     engine.runAndWait()
-try:
-    with sr.Microphone() as source:
-        print("Listening.......")
 
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'alexa' in command:
-            talk(command)
+def take_command():
+    command =""
+    try:
+        with sr.Microphone() as source:
+            print("Listening.......")
 
-except Exception as e:
-    print(e)
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+
+    except Exception as e:
+        print(e)
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing'+ song)
+        pywhatkit.playonyt(song)
+
+run_alexa()
